9ea494c9412c9a6f0db102159c64f8446f51935d
daigakuimo <enaena128@outlook.jp>
2019-11-21 12:51:38 +0900

    サーバ接続処理を関数化

--- client.c
+++ client.c
@@ -10,6 +10,7 @@
 #define CONTINUE 2
 int board[BOARD_MAX][BOARD_MAX] = {{0}};
 
+int  init(char *a[],SOCKET *s);
 void display();
 void decisionMyPut(char *input);
 int  insertBoard(char *data, int player);
@@ -17,31 +18,10 @@ int  insertBoard(char *data, int player);
 
 int main(int argc, char *argv[]) {	
 
-	//接続するサーバの情報の構造体を用意
-	struct sockaddr_in dest;
-	memset(&dest, 0, sizeof(dest));
-	
-	//サーバの情報を入力
-	char destination[256];;  //アドレス
-	strcpy(destination,argv[1]);
-	dest.sin_port = htons(atof(argv[2]));  //ポート番号
-	dest.sin_family = AF_INET;
-	dest.sin_addr.s_addr = inet_addr(destination);
+	//サーバーに接続
+	SOCKET s;
+	init(argv, &s);
 
-	//ソケット通信の準備・生成
-	WSADATA data;
-	WSAStartup(MAKEWORD(2, 0), &data);
-	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
-
-	//サーバへの接続
-	if (connect(s, (struct sockaddr *) &dest, sizeof(dest))) {
-
-		printf("%sに接続できませんでした\n", destination);
-		return -1;
-	}
-
-	printf("%sに接続しました\n", destination);
-	
 	char buffer[1024];
 	//サーバからデータを受信(名前入力を促される)
 	recv(s, buffer, 1024, 0);
@@ -83,6 +63,36 @@ int main(int argc, char *argv[]) {
 	WSACleanup();
 	return 0;
 
+}
+
+//サーバ接続
+int init(char *a[], SOCKET *s)
+{
+    //接続するサーバの情報の構造体を用意
+	struct sockaddr_in dest;
+	memset(&dest, 0, sizeof(dest));
+	
+	//サーバの情報を入力
+	char destination[256];;  //アドレス
+	strcpy(destination,a[1]);
+	dest.sin_port = htons(atof(a[2]));  //ポート番号
+	dest.sin_family = AF_INET;
+	dest.sin_addr.s_addr = inet_addr(destination);
+
+	//ソケット通信の準備・生成
+	WSADATA data;
+	WSAStartup(MAKEWORD(2, 0), &data);
+	*s = socket(AF_INET, SOCK_STREAM, 0);
+
+	//サーバへの接続
+	if (connect(*s, (struct sockaddr *) &dest, sizeof(dest))) {
+
+		printf("%sに接続できませんでした\n", destination);
+		return -1;
+	}
+
+	printf("%sに接続しました\n", destination);
+	
 }
 
 //盤面を表示
--- pra.c
+++ pra.c
@@ -1,16 +1,61 @@
+//http://blog.livedoor.jp/akf0/archives/51585502.html
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <time.h>
+#include <winsock2.h>
 
-void init(char *ar[]);
+int init(char *a[],SOCKET *s);
 
-int main(int argc, char *argv[]) 
-{	
-    init(argv);
+int main(int argc, char *argv[]) {
 
-    return 0;
+    SOCKET s;
+
+    init(argv,&s);	
+
+	char buffer[1024];
+	//サーバからデータを受信(名前入力を促される)
+	recv(s, buffer, 1024, 0);
+	printf("→ %s\n\n", buffer);
+
+	//名前入力
+	char input[256];
+	scanf("%s",input);	
+	send(s, input, strlen(input), 0);
+
+	// Windows でのソケットの終了
+	closesocket(s);
+	WSACleanup();
+	return 0;
+
 }
 
-void init(char *ar[])
+int init(char *a[], SOCKET *s)
 {
-    printf("%s",ar[1]);
+    //接続するサーバの情報の構造体を用意
+	struct sockaddr_in dest;
+	memset(&dest, 0, sizeof(dest));
+	
+	//サーバの情報を入力
+	char destination[256];;  //アドレス
+	strcpy(destination,a[1]);
+	dest.sin_port = htons(atof(a[2]));  //ポート番号
+	dest.sin_family = AF_INET;
+	dest.sin_addr.s_addr = inet_addr(destination);
+
+	//ソケット通信の準備・生成
+	WSADATA data;
+	WSAStartup(MAKEWORD(2, 0), &data);
+	*s = socket(AF_INET, SOCK_STREAM, 0);
+
+	//サーバへの接続
+	if (connect(*s, (struct sockaddr *) &dest, sizeof(dest))) {
+
+		printf("%sに接続できませんでした\n", destination);
+		return -1;
+	}
+
+	printf("%sに接続しました\n", destination);
+	
 }
+
