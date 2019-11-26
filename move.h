9463b361d135c0ed6aaafb1a04b0b5b73fa22fc3
daigakuimo <enaena128@outlook.jp>
2019-11-26 22:43:08 +0900

    comment

--- move.h
+++ move.h
@@ -4,7 +4,7 @@
 #define MOVE_MY_WIN              30    //勝ち確
 #define MOVE_PREVENT_ENEMY_WIN   25    //相手の勝ちを防ぐ
 #define MOVE_MY_CHECKMATE        20    //自分が王手
-#define MOVE_MY_FORBIDDEN_MOVE   0    //
+#define MOVE_MY_FORBIDDEN_MOVE   0     //禁じ手
 
 void move();
 int  maxLevel(int limit);
