d08e49ef2912ae283b5bc271a72b102474354aff
daigakuimo <enaena128@outlook.jp>
2019-11-26 22:38:52 +0900

    評価値増やした

--- move.h
+++ move.h
@@ -1,9 +1,10 @@
 #pragma once
 
 //評価値
-#define MOVE_MY_WIN              15    //勝ち確
-#define MOVE_PREVENT_ENEMY_WIN   14    //相手の勝ちを防ぐ
-#define MOVE_MY_CHECKMATE        13    //自分が王手
+#define MOVE_MY_WIN              30    //勝ち確
+#define MOVE_PREVENT_ENEMY_WIN   25    //相手の勝ちを防ぐ
+#define MOVE_MY_CHECKMATE        20    //自分が王手
+#define MOVE_MY_FORBIDDEN_MOVE   0    //
 
 void move();
 int  maxLevel(int limit);
